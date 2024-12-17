bb0dee1a3db2c00626ec5ff7c7ae4c48e3af4ad7
Sigma3.0 <141695117+Anki2616@users.noreply.github.com>
2024-12-17 18:10:41 +0530

    DSA_Series

---
+++ src/Dynamic_programming/Min_Jumps.java
@@ -0,0 +1,30 @@
+package Dynamic_programming;
+
+import java.util.Arrays;
+
+public class Min_Jumps {
+    public static int minJumps(int num[]){
+        int n = num.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+        dp[n-1] = 0;
+        for (int i=n-2;i>=0;i--){
+            int steps = num[i];
+            int ans = Integer.MAX_VALUE;
+            for (int j=i+1;j<=i+steps && j<n;j++){
+                if (dp[j]!=-1){
+                    ans = Math.min(ans,dp[j]+1);
+                }
+            }
+            if (ans!=Integer.MAX_VALUE){
+                dp[i] = ans;
+            }
+        }
+
+       return dp[0];
+    }
+    public static void main(String[] args) {
+        int nums[] = {2,3,1,1,4};
+        System.out.println("Total Minimum Jumps ->"+minJumps(nums));
+    }
+}
